8a8ad9c0fe5df935f5f722a14f43ee7f9ba1ee6a
IrinaProkofieva <athos_holmes@mail.ru>
2021-04-14 12:48:08 +0400

    Переструктурировала проект

--- tmp/client/src/pages/ContactPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const ContactPage = () => {
-	return (
-		<div>
-			<h1>Наши контакты</h1>
-		</div>
-	)
-}
--- tmp/client/src/pages/ProjectPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const ProjectPage = () => {
-	return (
-		<div>
-			<h1>Наши проекты</h1>
-		</div>
-	)
-}
