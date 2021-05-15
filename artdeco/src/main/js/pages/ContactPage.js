5cfe6fd10730f88d17c284e97ee43a5c563179e5
GorshkovaTatiana <79208495+GorshkovaTatiana@users.noreply.github.com>
2021-05-15 17:20:53 +0400

    Map

--- artdeco/src/main/js/pages/ContactPage.js
+++ artdeco/src/main/js/pages/ContactPage.js
@@ -6,8 +6,7 @@ export const ContactPage = () => {
 		<div>
 			<h1>Наши контакты</h1>
 			<p>
-			<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A733571b672e96fc1a54598bcdfe35f1d89a32fbd9717a1b8ac1d1b9a00399e01&amp;width=483&amp;height=384&amp; lang=ru_RU&amp; scroll=true>
-			</script>
+			<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A733571b672e96fc1a54598bcdfe35f1d89a32fbd9717a1b8ac1d1b9a00399e01&amp;width=483&amp;height=384&amp;lang=ru_RU&amp;scroll=true"></script>
 			</p>
 		</div>
 		</>
