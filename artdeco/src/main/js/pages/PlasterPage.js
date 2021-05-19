81d3aa880a678068c1fb30146709973b5fabaead
GorshkovaTatiana <79208495+GorshkovaTatiana@users.noreply.github.com>
2021-05-19 10:46:31 +0400

    Our project galery

--- artdeco/src/main/js/pages/PlasterPage.js
+++ artdeco/src/main/js/pages/PlasterPage.js
@@ -20,43 +20,39 @@ export class PlasterPage extends Component{
 			<>
 			<div className="row">
               <div className="column">
-                <img src="img/Photo Art Deco/project/1/1.jpg" alt="1" onclick="openImg(this);">
+                <img src="img/Photo Art Deco/project/1/1.jpg" alt="1" onclick="openImg(this);"/>
               </div>
               <div className="column">
-                <img src="img/Photo Art Deco/project/1/2.jpg" alt="2" onclick="openImg(this);">
+                <img src="img/Photo Art Deco/project/1/2.jpg" alt="2" onclick="openImg(this);"/>
               </div>
               <div className="column">
-                <img src="img/Photo Art Deco/project/1/3.jpg" alt="3" onclick="openImg(this);">
+                <img src="img/Photo Art Deco/project/1/3.jpg" alt="3" onclick="openImg(this);"/>
               </div>
               <div className="column">
-                <img src="img/Photo Art Deco/project/1/4.jpg" alt="4" onclick="openImg(this);">
+                <img src="img/Photo Art Deco/project/1/4.jpg" alt="4" onclick="openImg(this);"/>
               </div>
             </div>
 
-            <!-- The expanding image container -->
+
             <div className="container1">
-              <!-- Close the image -->
+
               <span onclick="this.parentElement.style.display='none'" class="closebtn">&times;</span>
 
-              <!-- Expanded image -->
-              <img id="expandedImg" style="width:100%">
 
-              <!-- Image text -->
+              <img id="expandedImg" style="width:100%"/>
+
+
               <div id="imgtext"></div>
             </div>
-            <script>function myFunction(imgs) {
-                      // Get the expanded image
-                      var expandImg = document.getElementById("expandedImg");
-                      // Get the image text
-                      var imgText = document.getElementById("imgtext");
-                      // Use the same src in the expanded image as the image being clicked on from the grid
-                      expandImg.src = imgs.src;
-                      // Use the value of the alt attribute of the clickable image as text inside the expanded image
-                      imgText.innerHTML = imgs.alt;
-                      // Show the container element (hidden with CSS)
-                      expandImg.parentElement.style.display = "block";
-                    }
-            </script>
+            function openImg(imgs) {
+                                   var expandImg = document.getElementById("expandedImg");
+                                   var imgText = document.getElementById("imgtext");
+                                  expandImg.src = imgs.src;
+                                  imgText.innerHTML = imgs.alt;
+                                  expandImg.parentElement.style.display = "block";
+                                }
+
+
 
 
 				<div className="container">
